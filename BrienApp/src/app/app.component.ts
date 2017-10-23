2fed57fd658c73740d608e02c64f4bd7fe2caead
SpydaBeast <brien@artofmars.org>
2017-10-22 19:52:49 -0700

    Added About component

--- BrienApp/src/app/app.component.ts
+++ BrienApp/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: '<h1>Brien Calloway</h1> <app-about></app-about>'
+              
 })
 export class AppComponent {
   title = 'app';
