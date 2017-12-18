db8e95ccafac760bba61872db5d613f85bbfc3c2
SpydaBeast <brien@artofmars.org>
2017-12-17 21:49:29 -0800

    added material module and animation libraries. Also added a collections component

--- MiniLibrary/src/app/app.module.ts
+++ MiniLibrary/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { BrowerAnimationModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { CollectionComponent } from './collection/collection.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    CollectionComponent
 ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowerAnimationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
