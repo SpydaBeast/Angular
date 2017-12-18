fcd00084a5512bb2c19698c93f289f695e8ee099
SpydaBeast <brien@artofmars.org>
2017-12-17 20:56:58 -0800

    added about page as root

--- MiniLibrary/src/app/app.component.ts
+++ MiniLibrary/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: '<h1> My Tiny Library App</h1> <app-about></app-about>'
+  
 })
 export class AppComponent {
   title = 'app';
