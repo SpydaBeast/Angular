39e1e3eeeb7ef0053c7e78e9c67d7fe7e3e63a06
SpydaBeast <brien@artofmars.org>
2017-12-17 22:25:19 -0800

    Started implementation of the IBook interface. Still having trouble implementing the Materials library, and the Browser animation.

--- MiniLibrary/src/app/app.module.ts
+++ MiniLibrary/src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material';
-import { BrowerAnimationModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { CollectionComponent } from './collection/collection.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     CollectionComponent
-],
+  ],
   imports: [
     BrowserModule,
     FormsModule,
-    MaterialModule,
-    BrowerAnimationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MiniLibrary/src/app/collection/collection.component.ts
+++ MiniLibrary/src/app/collection/collection.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-collection',
+  selector: 'my-collection',
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.css']
 })
 export class CollectionComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.startTime = new Date();
+    this.startTime.setHours(10, 0);
+    this.endTime = new Date();
+    this.endTime.setHours(15, 0);
+  }
 
   ngOnInit() {
   }
+
+  pageTitle:string = 'Books';
+
+  startTime:Date;
+  endTime:Date;
+
+  showOperatingHours: boolean = false;
 
 }
