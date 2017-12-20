cb6c34f891766448025d330e58b0558858149594
SpydaBeast <brien@artofmars.org>
2017-12-19 19:59:48 -0800

    Added collection IBook import to collection.ts

--- MiniLibrary/src/app/app.module.ts
+++ MiniLibrary/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material'
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -17,8 +18,9 @@ import { CollectionComponent } from './collection/collection.component';
   imports: [
     BrowserModule,
     FormsModule,
-    MatButtonModule, 
-    MatCheckboxModule
+    MatButtonModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MiniLibrary/src/app/ibook.ts
+++ MiniLibrary/src/app/ibook.ts
@@ -1,2 +1,2 @@
-export interface Ibook {
+export interface IBook {
 }
